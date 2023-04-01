e8bef76ee48d8250c32587014f334e0826875f13
Daim Shahzad <127994956+daimshahzad69@users.noreply.github.com>
2023-04-01 16:31:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+  alert ("hello world")
+  console.log("hey")
+
+  var a=20
+  var b=20
+  var c=24
+  var d=35
+  var z=a*c
+ var x=a+d+c
+  console.log(z)
+  console.log(x)
+
+var b = 12
+var a = 76
+var n = 23
+var l = 21
+var z = (b*l)-a/n 
+console.log(z)
+
+
+ var a=78
+ var b=54
+ var t=++a
+ var d=--b
+
+ console.log(t)
+ console.log(d)
+
+var firstname = prompt("Enter First Name")
+var lastname = prompt("Enter last Name")
+var city = prompt("Enter city", "lahore")
+
+console.log(firstname + " " + lastname + " from " + city)
+
+var agrhamareammalachehein = false
+
+if(agrhamareammalachehein == false){
+    console.log("humme bht fikr krni chaiye")
+} else{
+    console.log("humme unhe or ache krne ki koshish krni chaiye")
+}
+
+
+
+
